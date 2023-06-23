9e81e2e878a1d8a9e7846e3d7c821a20c947dbe2
Nishita1502 <123340115+Nishita1502@users.noreply.github.com>
2023-06-22 22:28:52 -0400

    Add files via upload

---
+++ Patient.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Assignment_1
+{
+	public class Patient
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public int Weight { get; set; }
+		public int Height { get; set; }
+
+		//custom contror to create new patient with all properties value set 
+		public Patient(string firstName, string lastName, int weight, int height)
+		{
+			FirstName = firstName;
+			LastName = lastName;
+			Weight = weight;
+			Height = height;
+		}
+
+		public string GetBloodPressureResult(int systolic, int diastonic)
+		{
+			string bloodPressureCategory = string.Empty;
+			if (systolic < 120 && diastonic < 80)
+			{
+				bloodPressureCategory = "NORMAL";
+			}
+			else if (systolic <= 129 && diastonic < 80)
+			{
+				bloodPressureCategory = "ELEVATED";
+			}
+			else if (systolic <= 139 && diastonic <= 89)
+			{
+				bloodPressureCategory = "HIGH BLOOD PRESSURE (HYPERTENSION) STAGE 1";
+			}
+			else if (systolic <= 139 && diastonic <= 89)
+			{
+				bloodPressureCategory = "HIGH BLOOD PRESSURE (HYPERTENSION) STAGE 1";
+			}
+			else if (systolic <= 180 && diastonic <= 120)
+			{
+				bloodPressureCategory = "HIGH BLOOD PRESSURE (HYPERTENSION) STAGE 2";
+			}
+			else if (systolic > 180 && diastonic > 120)
+			{
+				bloodPressureCategory = "HYPERTENSIVE CRISIS (consult your doctor immediately)";
+			}
+			else
+			{
+				bloodPressureCategory = "Invalid Input. Please Enter Proper value.";
+			}
+
+			return bloodPressureCategory;
+		}
+
+		public double CalculateBMI()
+		{
+			double m = Height / 100.00;
+			double bmi = Weight / (m * m);
+			return bmi;
+		}
+
+		public void ShowPatientInformation(int systolic, int diastonic)
+		{
+			double bmi = CalculateBMI();
+			string bmiStatus = string.Empty;
+			if (bmi >= 25.0)
+			{
+				bmiStatus= "Overweight";
+			}
+			else if (bmi >= 18.5 && bmi <= 24.9)
+			{
+				bmiStatus = "Normal (In the healthy range)";
+			}
+			else
+			{
+				bmiStatus = "Underweight";
+			}
+
+			Console.WriteLine("Full Name: " + FirstName + " " + LastName);
+			Console.WriteLine("Weight: " + Weight);
+			Console.WriteLine("Height: " + Height);
+			Console.WriteLine("Blood Pressure Result: " + GetBloodPressureResult(systolic, diastonic));
+			Console.WriteLine("BMI Numeric Value: " + bmi);
+			Console.WriteLine("BMI Status: " + bmiStatus);
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Assignment_1
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Enter your age: ");
+			int age = Convert.ToInt32(Console.ReadLine());
+
+			Console.Write("Enter the patient's first name: ");
+			string firstName = Console.ReadLine();
+
+			Console.Write("Enter the patient's last name: ");
+			string lastName = Console.ReadLine();
+
+			Console.Write("Enter the patient's weight (in kg): ");
+			int weight = Convert.ToInt32(Console.ReadLine());
+
+			Console.Write("Enter the patient's height (in cm): ");
+			int height = Convert.ToInt32(Console.ReadLine());
+
+			Console.Write("Enter the patient's SYSTOLIC mm Hg (upper number): ");
+			int systolic = Convert.ToInt32(Console.ReadLine());
+
+			Console.Write("Enter the patient's DIASTOLIC mm Hg (lower number): ");
+			int diastonic = Convert.ToInt32(Console.ReadLine());
+
+			// Create an instance of the Patient class using the provided information
+			Patient patient = new Patient(firstName, lastName, weight, height);
+			
+			Console.WriteLine("=====================================");
+			Console.WriteLine("Patient Information:");
+			Console.WriteLine("=====================================");
+			patient.ShowPatientInformation(systolic, diastonic);
+
+			Console.ReadLine();
+		}
+	}
+}
